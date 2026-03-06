c296df4ccfeddb963f305e3e5b789ea6952201c1
MK <phamkhoi944@gmail.com>
2026-03-06 17:07:11 +0700

    update fuel and wall

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -2,35 +2,64 @@ using UnityEngine;
 using UnityEngine.UIElements;
 public class GameManager : MonoBehaviour
 {
-    public float thoigianmax=0f;
+    public float thoigianmax = 0f;
 
     public UIDocument UIdoc; // Tham chiếu đến UI Document chứa Text để hiển thị điểm số
     private Label MaxScore;
+    public GameObject rock;
+    public GameObject Fuel;
+    public float timeSpawn = 1.5f;
+    public float gioihanY = 6.5f;
+    public float vitrispawnX = 20f;
+    private float timeCount = 0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        thoigianmax=PlayerPrefs.GetFloat("HighScore",0f); // Lấy điểm cao nhất đã lưu, mặc định là 0 nếu chưa có
-        MaxScore=UIdoc.rootVisualElement.Q<Label>("MaxScore");
+        thoigianmax = PlayerPrefs.GetFloat("HighScore", 0f); // Lấy điểm cao nhất đã lưu, mặc định là 0 nếu chưa có
+        MaxScore = UIdoc.rootVisualElement.Q<Label>("MaxScore");
         end_work(thoigianmax); // Hiển thị điểm cao nhất trên UI khi bắt đầu trò chơi
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeCount += Time.deltaTime;
+        if (timeCount >= timeSpawn)
+        {
+            timeCount = 0f;
+            taoRock();
+        }
+        if (timeCount >= timeSpawn * 2f)
+        {
+            timeCount = 0f;
+            FuelSpawn();
+        }
     }
     public void end_work(float thoigian)
     {
-        if (thoigian>thoigianmax)
+        if (thoigian > thoigianmax)
         {
             PlayerPrefs.SetFloat("HighScore", thoigian); // Lưu điểm cao nhất mới nếu vượt qua điểm cũ
 
             PlayerPrefs.Save();
 
-            MaxScore.text="New High Score: "+thoigian; // Cập nhật điểm cao nhất trên UI
-        } else
-        {
-            MaxScore.text="High Score: "+thoigianmax; // Hiển thị điểm cao nhất hiện tại trên UI    
+            MaxScore.text = "New High Score: " + thoigian; // Cập nhật điểm cao nhất trên UI
         }
+        else
+        {
+            MaxScore.text = "High Score: " + thoigianmax; // Hiển thị điểm cao nhất hiện tại trên UI    
+        }
+    }
+    void taoRock()
+    {
+        float y = Random.Range(-gioihanY, gioihanY);
+        Vector3 vitriSpawn = new Vector3(vitrispawnX, y, 0f);
+        Instantiate(rock, vitriSpawn, Quaternion.identity);
+    }
+    void FuelSpawn()
+    {
+        float y = Random.Range(-gioihanY, gioihanY);
+        Vector3 vitriSpawn = new Vector3(vitrispawnX, y, 0f);
+        Instantiate(Fuel, vitriSpawn, Quaternion.identity);
     }
 }
--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using System.Runtime.CompilerServices;
 
 public class PlayerController : MonoBehaviour
 {
@@ -21,6 +22,17 @@ public class PlayerController : MonoBehaviour
     public GameManager gameManager; // Tham chiếu đến GameManager để cập nhật điểm số
     public AudioSource EngineSound;
 
+    [Header("Cai đặt Giới Hạn Di Chuyển")]
+    private float minX = -12f;
+    private float maxX = 14f;
+    private float minY = -7.5f;
+    private float maxY = 7.5f;
+
+    [Header("Cài đặt Nhiên Liệu")]
+    public float maxFuel = 100f;
+    public float currentFuel = 100f;
+    private VisualElement fuelFill;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -29,6 +41,7 @@ public class PlayerController : MonoBehaviour
         Restart = UIdoc.rootVisualElement.Q<Button>("Restart_Button"); // Tìm Button trong UI Document
         Restart.style.display = DisplayStyle.None; // Ẩn nút Restart
         Restart.clicked += RestartGame; // Gán sự kiện click cho nút Restart
+        fuelFill = UIdoc.rootVisualElement.Q<VisualElement>("Fuel_Fill");
     }
 
     // Update is called once per frame
@@ -36,6 +49,8 @@ public class PlayerController : MonoBehaviour
     {
         MovePlayer();
         UpdateScore();
+        gioihan();
+
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
@@ -54,42 +69,65 @@ public class PlayerController : MonoBehaviour
         scoreText.text = "Score: " + score; //Cập nhật điểm số trên UI
     }
     private void MovePlayer()
-{
-    // 1. NẾU ĐANG GIỮ CHUỘT
-    if (Mouse.current.leftButton.isPressed)
     {
-        Vector3 mousepos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
-        Vector2 direction = mousepos - transform.position; 
-        transform.up = direction; 
-        rb.AddForce(direction.normalized * speed); 
-
-        if (rb.linearVelocity.magnitude > maxSpeed)
+        // 1. NẾU ĐANG GIỮ CHUỘT
+        if (Mouse.current.leftButton.isPressed && currentFuel > 0)
         {
-            rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed; 
+            Vector3 mousepos = Camera.main.ScreenToWorldPoint(Mouse.current.position.value);
+            Vector2 direction = mousepos - transform.position;
+            transform.up = direction;
+            rb.AddForce(direction.normalized * speed);
+
+            if (rb.linearVelocity.magnitude > maxSpeed)
+            {
+                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+            }
+
+            flameEffect.SetActive(true);
+            ExhaustEffect.SetActive(true);
+
+            // Bật âm thanh nếu nó chưa chạy
+            if (EngineSound.isPlaying == false)
+            {
+                EngineSound.Play();
+            }
+            currentFuel -= Time.deltaTime * 10f;
+            float FuelPercentage = (currentFuel / maxFuel) * 100f;
+            if (fuelFill != null)
+            {
+                fuelFill.style.height = new Length(FuelPercentage, LengthUnit.Percent);
+            }
         }
+        // 2. NẾU KHÔNG GIỮ CHUỘT (hoặc vừa nhả ra)
+        else
+        {
+            flameEffect.SetActive(false);
+            ExhaustEffect.SetActive(false);
 
-        flameEffect.SetActive(true);
-        ExhaustEffect.SetActive(true);  
-
-        // Bật âm thanh nếu nó chưa chạy
-        if (EngineSound.isPlaying == false) { 
-            EngineSound.Play(); 
+            // Tắt âm thanh
+            if (EngineSound.isPlaying == true)
+            {
+                EngineSound.Stop();
+            }
         }
     }
-    // 2. NẾU KHÔNG GIỮ CHUỘT (hoặc vừa nhả ra)
-    else 
-    { 
-        flameEffect.SetActive(false); 
-        ExhaustEffect.SetActive(false); 
-
-        // Tắt âm thanh
-        if (EngineSound.isPlaying == true) {
-            EngineSound.Stop();
-        }
-    }
-}
     private void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Tải lại scene hiện tại để bắt đầu lại trò chơi
+    }
+    private void gioihan()
+    {
+        Vector3 vitrihientai = transform.position;
+        vitrihientai.x = Mathf.Clamp(vitrihientai.x, minX, maxX);
+        vitrihientai.y = Mathf.Clamp(vitrihientai.y, minY, maxY);
+        transform.position = vitrihientai;
+    }
+    void OnTriggerEnter2D(Collider other)
+    {
+        if (other.gameObject.CompareTag("Fuel"))
+        {
+            currentFuel += 20f;
+            Destroy(other.gameObject);
+        }
     }
 }
